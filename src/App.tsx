6ab2e443116286ae5e6589a72cdb414a9a5ca547
VadymDenysiuk <80970410371q@gmail.com>
2024-12-08 14:03:37 +0200

    add redirect to the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import List from "./pages/List";
 
@@ -8,7 +13,8 @@ const App: React.FC = () => {
     <Router>
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Navigate replace to="/main" />} />
+          <Route path="/main" element={<Home />} />
           <Route path="/list" element={<List />} />
         </Routes>
       </main>
--- src/components/List/Header.tsx
+++ src/components/List/Header.tsx
@@ -4,7 +4,7 @@ import { ReactComponent as BackIcon } from "../../assets/svg/back.svg";
 const Header: React.FC = () => {
   return (
     <div className="header">
-      <a href="/" className="header__back">
+      <a href="/main" className="header__back">
         <BackIcon />
       </a>
       <h3 className="header__title">Weather history</h3>
